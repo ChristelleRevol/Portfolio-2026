dd5503aa8cfc8a462112527e432662d4c54e57b2
Christelle Revol <christellerevol.works@gmail.com>
2026-05-25 17:05:59 +0200

    fix error deploy

--- portfolio-2026-backend/server.js
+++ portfolio-2026-backend/server.js
@@ -3,6 +3,9 @@ import nodemailer from 'nodemailer'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config()
 
 const app = express()
@@ -31,4 +34,20 @@ app.post('/contact', async (req, res) => {
   res.status(200).json({ success: true })
 })
 
-app.listen(5000, () => console.log('Serveur démarré sur le port 5000'))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
+
+// --------------------------
+// pour le déploiement      |
+// --------------------------
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Sert les fichiers statiques du build React
+app.use(express.static(path.join(__dirname, "../portfolio-2026/dist")));
+
+// Renvoie index.html pour toutes les routes React (client-side routing)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../portfolio-2026/dist", "index.html"));
+});
